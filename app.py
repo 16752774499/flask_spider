23d6299e704a2fc2f9ba83aeb978bfae8e9cd9ed
XIAOHUA\27553 <2755319956@qq.com>
2024-06-19 22:27:29 +0800

    增加debug=off模式下页面无法响应或服务器出错页面。

--- app.py
+++ app.py
@@ -6,13 +6,27 @@ from routes import adminRoutes
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 
 CORS(app, supports_credentials=True)
 
 # 注册路由
 app.register_blueprint(Routes.routes_module)
 app.register_blueprint(adminRoutes.routes_module)
+
+
+# 自定义出错处理页面
+@app.errorhandler(404)
+def page_not_found(e):
+    print(e)
+    return render_template('404.html', msg="该页面不存在！", state="404"), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    print(e)
+    return render_template('404.html', msg="服务器链接异常！", state="500"), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
